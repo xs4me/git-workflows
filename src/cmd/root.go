a8b5c035cb6ec792775236e4ecac0e05d03f4ecc
fhochleitner <felix.hochleitner@outlook.com>
2023-02-15 16:55:31 +0100

    GPX-598 [CICD] Refactoring "git-workflows" Skript in CLI Applikation
    add missing configuration parameter
    
    Signed-off-by: fhochleitner <felix.hochleitner@outlook.com>

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -49,6 +49,7 @@ func init() {
 	rootCmd.PersistentFlags().StringVar(&Config.TagLocation, "image-tag-location", "", "Location of image-tag in the infrastructure repository")
 	rootCmd.PersistentFlags().StringSliceVar(&Config.Stages, "stages", []string{"main", "dev", "qa", "prod"}, "deployment stages")
 	rootCmd.PersistentFlags().StringVar(&Config.FromBranch, "from-branch", "main", "Base branch for argo-create | Branch from which to deploy from")
+	rootCmd.PersistentFlags().StringVar(&Config.FromBranch, "to-branch", "", "Target brancht for deployments")
 
 	deleteCmd.PersistentFlags().BoolVarP(&Config.Force, "force", "f", false, "allows deletion of protected environments. Remember: with great power comes great responsibility!")
 }
