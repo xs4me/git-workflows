e2da5b863d49ce43916115972ade905572f58b6b
argo-ci <argo-ci@geaprdec.com>
2023-02-23 15:40:54 +0100

    GPX-598 [CICD] Refactoring "git-workflows" Skript in CLI Applikation
    move yaml ops to seperate go file.
    
    Signed-off-by: argo-ci <argo-ci@geaprdec.com>

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ## todo
 
-make it work root less with /root/.ssh/ folder as random uid
+* make it work root less with /root/.ssh/ folder as random uid
+* make it possible to change templates and values for defined stages without changing the deployed image
+* write tests
+* properly implement merging for deployment of application
+* remove yq for updates at applicationset
--- src/api/argo.go
+++ src/api/argo.go
@@ -1,15 +1,12 @@
 package api
 
 import (
-	"bytes"
 	"fmt"
 	"gepaplexx/git-workflows/logger"
 	"gepaplexx/git-workflows/model"
 	"gepaplexx/git-workflows/utils"
 	"github.com/go-git/go-git/v5"
 	"github.com/otiai10/copy"
-	"gopkg.in/yaml.v3"
-	"io"
 	"os"
 	"os/exec"
 	"strings"
@@ -94,89 +91,6 @@ func updateImageTag(c *model.Config, filepath string) {
 	}
 
 	writeUpdatedYaml(nodes, filepath)
-}
-
-func parseYaml(filepath string) yaml.Node {
-	file, err := os.Open(filepath)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	by, err := io.ReadAll(file)
-	utils.CheckIfError(err)
-
-	var node yaml.Node
-	err = yaml.Unmarshal(by, &node)
-	utils.CheckIfError(err)
-	return node
-}
-
-func writeUpdatedYaml(nodes yaml.Node, filepath string) {
-	var b bytes.Buffer
-	yamlEncoder := yaml.NewEncoder(&b)
-	yamlEncoder.SetIndent(2)
-	err := yamlEncoder.Encode(&nodes)
-	utils.CheckIfError(err)
-
-	err = os.WriteFile(filepath, b.Bytes(), 0664)
-	utils.CheckIfError(err)
-}
-func updateVal(node *yaml.Node, updatePath string, newVal string) bool {
-	current := ""
-	found := false
-	update(node, &current, updatePath, newVal, &found)
-	return found
-}
-
-func update(node *yaml.Node, current *string, lookingFor string, newVal string, found *bool) {
-	if *found {
-		return
-	}
-	if node.Kind == yaml.SequenceNode {
-		for _, child := range node.Content {
-			update(child, current, lookingFor, newVal, found)
-		}
-	} else if node.Kind == yaml.MappingNode {
-		for i := 0; i < len(node.Content); i += 2 {
-			key := node.Content[i]
-			value := node.Content[i+1]
-			appendIfValid(current, key.Value, lookingFor)
-			if *current == lookingFor {
-				node.Content[i+1].Value = newVal
-				*found = true
-				return
-			}
-			update(key, current, lookingFor, newVal, found)
-			update(value, current, lookingFor, newVal, found)
-
-		}
-	} else {
-		if *current == lookingFor {
-			node.Value = newVal
-			*found = true
-		}
-
-		appendIfValid(current, node.Value, lookingFor)
-	}
-}
-
-func appendIfValid(current *string, appendix string, lookingFor string) {
-	newCurrent := *current
-	if *current != "" {
-		newCurrent += "."
-	}
-
-	newCurrent += appendix
-	if !strings.HasPrefix(lookingFor, newCurrent) {
-		if *current != "" {
-			newCurrent = strings.TrimSuffix(newCurrent, "."+appendix)
-		} else {
-			newCurrent = ""
-		}
-	}
-
-	*current = newCurrent
 }
 
 func addEnvironmentToApplicationSet(c *model.Config, path string) {
