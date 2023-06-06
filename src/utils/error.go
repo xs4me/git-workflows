9f028944cd482677237e70337529d118582f4093
fhochleitner <felix.hochleitner@gepardec.com>
2023-06-06 08:43:11 +0200

    GPX-692 [CICD] Create Workflow - Umgebung kann mehrmals hinzugefügt werden
    Added check for existing node before adding new environment and cancel execution in case the node already exists
    
    Signed-off-by: fhochleitner <felix.hochleitner@gepardec.com>

--- src/utils/error.go
+++ src/utils/error.go
@@ -5,11 +5,12 @@ import (
 )
 
 const AlreadyUpToDateErr = "already up-to-date"
+const ElementNotFoundErr = "element not found"
 
 func CheckIfError(err error) {
 	if err != nil {
-		if err.Error() == AlreadyUpToDateErr {
-			logger.Info(AlreadyUpToDateErr)
+		if err.Error() == AlreadyUpToDateErr || err.Error() == ElementNotFoundErr {
+			logger.Info(err.Error())
 			return
 		}
 		logger.Fatal(err.Error())
